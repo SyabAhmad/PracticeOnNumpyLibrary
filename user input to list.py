25c268f27afc74044500cc39dac88e2834ac6d80
SyabAhmad <syabblogger@gmail.com>
2023-03-21 21:45:40 +0500

    update changes

--- user input to list.py
+++ user input to list.py
@@ -2,7 +2,7 @@ import numpy as np
 
 print("User input to List")
 data = np.array([])
-dt1 = input("Enter Values to Append to List")
+dt1 = input("Enter Values to fill up List")
 data.fill(dt1)
 
 print(data)

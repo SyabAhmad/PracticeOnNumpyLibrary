18a976ceeed3db90858b64f3e0799b4bc6b895b8
SyabAhmad <syabblogger@gmail.com>
2023-03-22 11:49:10 +0500

    main updated

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# NumPy is an open-source Python library for performing
+# array computing (matrix operations). It can be used to
+# perform a wide variety of mathematical operations on arrays
+# and matrices. NumPy objects can be easily converted to other
+# types of objects like the Pandas data frame and the tensorflow tensor.
+#
+# NumPy provides a large number of mathematical functions that
+# can be performed on arrays and matrices. These functions include
+# trigonometric, algebraic, and statistical operations. Some of the
+# mathematical functions provided by NumPy are:
+#
+# - `add(): Adds two arrays element-wise.
+# - `subtract(): Subtracts two arrays element-wise.
+# - `multiply(): Multiplies two arrays element-wise.
+# - `divide(): Divides two arrays element-wise.
+# - `power(): Raises the elements of an array to a specified power.
+# - `sqrt(): Computes the square root of each element in an array.
+# - `exp(): Computes the exponential of each element in an array.
+# - `log(): Computes the natural logarithm of each element in an array.
+# - `sin(): Computes the sine of each element in an array.
+# - `cos(): Computes the cosine of each element in an array.
+# - `tan(): Computes the tangent of each element in an array.
+# - `arcsin(): Computes the inverse sine of each element in an array.
+# - `arccos(): Computes the inverse cosine of each element in an array.
+# - `arctan(): Computes the inverse tangent of each element in an array.
+# - `dot(): Computes the dot product of two arrays.
